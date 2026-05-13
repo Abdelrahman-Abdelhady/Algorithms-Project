6b33c8b4b3e07501cf344f7a70a9b20a8dfc5370
boodyxdgamer <boodyehab2006@gmail.com>
2026-05-13 23:54:28 +0300

    Update BirthdayCakeCandels.cpp

--- BirthdayCakeCandels/BirthdayCakeCandels.cpp
+++ BirthdayCakeCandels/BirthdayCakeCandels.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-
+/*--------------------------------------Algorithm 1 Non-Recursive--------------------------------------*/
 
 int birthdayCakeCandles1(int n, int candles[])
 {
@@ -29,6 +29,8 @@ int birthdayCakeCandles1(int n, int candles[])
 	}
 	return count;
 }
+
+/*--------------------------------------Algorithm 2 Recursive--------------------------------------*/
 
 
 int maxElement(int n , int arr[])
@@ -61,6 +63,69 @@ int birthdayCakeCandles2(int n, int arr[],int max = -1)
 
 }
 
+/*--------------------------------------Algorithm 3 Recursive(merge Sort)--------------------------------------*/
+
+// Merge two sorted halves
+void merge(int arr[], int left, int mid, int right) {
+    int tempSize = right - left + 1;
+    int* temp = new int[tempSize]; // temporary array to set sorted element
+    
+    int i = left;
+    int j = mid + 1;
+    int k = 0;// used in temporary array
+    
+    while (i <= mid && j <= right) {
+        if (arr[i] <= arr[j])
+            //add element in temp and incerement i
+        temp[k++] = arr[i++];
+        else
+           //add element in temp and incerement j
+        temp[k++] = arr[j++];
+    }
+    //
+    while (i <= mid) {
+    temp[k++] = arr[i++];//add remaining elements from left half
+    }
+    while (j <= right) {
+        temp[k++] = arr[j++];//add remaining elements from right half
+    }
+    // Copy back to original array
+    for (int index = 0; index < tempSize; index++) {
+        arr[left + index] = temp[index];
+    }
+    
+    delete[] temp; // delete temporary array
+}
+
+// this func sorted array
+void mergeSort(int arr[], int left, int right) {
+    if (left >= right)
+     return;
+    
+    int mid = left + (right - left) / 2;
+    mergeSort(arr, left, mid); //sort left half
+    mergeSort(arr, mid + 1, right);//sort right half
+    merge(arr, left, mid, right);
+}
+
+// Birthday Cake Candles function
+int birthdayCakeCandles3( int size,int candles[]) {
+    if (size == 0)
+     return 0; 
+    
+    mergeSort(candles, 0, size - 1);
+    
+    int max = candles[size - 1]; // after sorting max in the last index
+    int count = 0;
+    
+    for (int i = size - 1; i >= 0; i--) {
+        if (candles[i] == max)
+            count++;
+        else
+            break;
+    }
+    return count;
+}
 
 //main function
 
